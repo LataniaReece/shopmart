e70f2ca8bda0b0aacab1eae2b55e486cab651501
LataniaReece <48932236+LataniaReece@users.noreply.github.com>
2021-12-14 10:38:59 -0500

    Update productReducers.js

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -3,6 +3,10 @@ import {
   PRODUCT_CREATE_REQUEST,
   PRODUCT_CREATE_RESET,
   PRODUCT_CREATE_SUCCESS,
+PRODUCT_UPDATE_FAIL,
+  PRODUCT_UPDATE_REQUEST,
+  PRODUCT_UPDATE_RESET,
+  PRODUCT_UPDATE_SUCCESS,
   PRODUCT_DELETE_FAIL,
   PRODUCT_DELETE_REQUEST,
   PRODUCT_DELETE_SUCCESS,
@@ -70,3 +74,5 @@ export const productCreateReducer = (state = {}, action) => {
       return state;
   }
 };
+
+export const productUpdateReducer = (state = {}, action) => { switch (action.type) { case PRODUCT_UPDATE_REQUEST: return { loading: true }; case PRODUCT_UPDATE_SUCCESS: return { loading: false, success: true, product: action.payload }; case PRODUCT_UPDATE_FAIL: return { loading: false, error: action.payload }; case PRODUCT_UPDATE_RESET: return {}; default: return state; }};
