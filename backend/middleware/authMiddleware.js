442b0640bb7ba6c6431c49780d9071236db1a6c6
YOUR_USERNAME <latania.reece@outlook.com>
2022-01-10 15:17:21 -0500

    capitalized model imports

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,28 +1,30 @@
-const jwt = require('jsonwebtoken')
-const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
 module.exports.verifyUser = async (req, res, next) => {
-    let token
+  let token;
 
-    try {
-
-        if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-            token = req.headers.authorization.split(' ')[1]
-            const decoded = jwt.verify(token, process.env.JWT_SECRET)
-            req.user = await User.findById(decoded.id).select('-password')
-            next()
-        } else {
-            return res.status(401).json({ message: 'Not authorized, no token' })
-        }
-    } catch (error) {
-        return res.status(401).json({ message: 'Not authorized, token failed' })
+  try {
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith('Bearer')
+    ) {
+      token = req.headers.authorization.split(' ')[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.id).select('-password');
+      next();
+    } else {
+      return res.status(401).json({ message: 'Not authorized, no token' });
     }
-}
+  } catch (error) {
+    return res.status(401).json({ message: 'Not authorized, token failed' });
+  }
+};
 
 module.exports.verifyAdmin = (req, res, next) => {
-    if (req.user && req.user.isAdmin) {
-        next()
-    } else {
-        return res.status(401).json({ message: 'Not authorized as an admin' })
-    }
-}
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    return res.status(401).json({ message: 'Not authorized as an admin' });
+  }
+};
--- backend/middleware/orderMiddleware.js
+++ backend/middleware/orderMiddleware.js
@@ -1,10 +1,12 @@
-const Order = require('../models/order');
+const Order = require('../models/Order');
 
-module.exports.isUserOrder = async(req, res, next) =>{
-    const id = req.params.id;
-    const order = await Order.findById(id);
-    if(!order.user.equals(req.user._id)){
-        return res.status(401).json({ message: 'You do not have permission to do that!' });
-    }
-    next();
+module.exports.isUserOrder = async (req, res, next) => {
+  const id = req.params.id;
+  const order = await Order.findById(id);
+  if (!order.user.equals(req.user._id)) {
+    return res
+      .status(401)
+      .json({ message: 'You do not have permission to do that!' });
+  }
+  next();
 };
