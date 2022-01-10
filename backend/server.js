f6c4b7a0308de5e70160068e45b307afff249c2f
YOUR_USERNAME <latania.reece@outlook.com>
2022-01-10 14:04:08 -0500

    first deploy attempt

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
-if(process.env.NODE_ENV !== 'production'){
-    require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
 }
 
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
-const cors = require('cors')
+const path = require('path');
+const cors = require('cors');
 
-const connectDB = require('./config/db.js')
+const connectDB = require('./config/db.js');
 
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/users');
@@ -16,8 +16,8 @@ const cartRoutes = require('./routes/carts');
 const orderRoutes = require('./routes/orders');
 const stripeRoutes = require('./routes/stripe');
 
-app.use(cors())
-app.use(express.urlencoded({extended: true}));
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 connectDB();
@@ -30,9 +30,26 @@ app.use('/api/orders', orderRoutes);
 app.use('/api/checkout', stripeRoutes);
 
 app.get('/', (req, res) => {
-    res.send('Api is running....')
-})
+  res.send('Api is running....');
+});
+
+var __dirname = path.resolve();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('Api is running....');
+  });
+}
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
