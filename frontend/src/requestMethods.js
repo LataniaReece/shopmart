129b3667349e4cc78dfe1f90962073917b52f302
YOUR_USERNAME <latania.reece@outlook.com>
2022-01-10 18:00:51 -0500

    updates

--- frontend/src/requestMethods.js
+++ frontend/src/requestMethods.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000';
+let BASE_URL;
+if (process.env.NODE_ENV === 'development') {
+  BASE_URL = 'http://localhost:5000';
+} else if (process.env.NODE_ENV === 'production') {
+  BASE_URL = '';
+}
 
 const user = JSON.parse(localStorage.getItem('userInfo'));
 
