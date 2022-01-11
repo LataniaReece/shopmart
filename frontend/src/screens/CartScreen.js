f12720e47df5fb8749ee581fdba5521ec72d0b85
YOUR_USERNAME <latania.reece@outlook.com>
2022-01-10 19:03:32 -0500

    updates

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -125,8 +125,10 @@ const CartScreen = () => {
                 image='https://cdn.pixabay.com/photo/2016/12/07/15/15/lotus-with-hands-1889661_960_720.png'
                 billingAddress
                 shippingAddress
-                description={`Your total is $${cart.total}`}
-                amount={cart.total * 100}
+                description={`Your total is $${
+                  (cart.total + 4.99 + 5.0) * 100
+                }`}
+                amount={(cart.total + 4.99 + 5.0) * 100}
                 token={onToken}
                 stripeKey={KEY}
               >
@@ -269,8 +271,10 @@ const CartScreen = () => {
                     image='https://cdn.pixabay.com/photo/2016/12/07/15/15/lotus-with-hands-1889661_960_720.png'
                     billingAddress
                     shippingAddress
-                    description={`Your total is $${cart.total}`}
-                    amount={cart.total * 100}
+                    description={`Your total is $${
+                      (cart.total + 4.99 + 5.0) * 100
+                    }`}
+                    amount={(cart.total + 4.99 + 5.0) * 100}
                     token={onToken}
                     stripeKey={KEY}
                   >
